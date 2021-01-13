c34a4230a825ea7af9d82b4572f492e751355a83
pythias <pythias@gmail.com>
2021-01-13 11:58:24 +0800

    广场也可以

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # weibo-bull
-微博福牛摸一摸
+
+微博福牛摸一摸 `DON'T BE EVIL`
 
 ## 使用方法
 
 1. 使用 chrome 打开 [微博](https://weibo.com) 并登录
 2. 访问页面 [https://huodong.weibo.cn/hongbao2021](https://huodong.weibo.cn/hongbao2021)
-2. 打开调试窗口，复制以下代码至console，执行既可
+3. 打开调试窗口，复制以下代码至console，执行既可
 
 ```js
 fetch("https://raw.githubusercontent.com/pythias/weibo-bull/master/weibo-bull.js")
--- weibo-bull.js
+++ weibo-bull.js
@@ -4,41 +4,37 @@
 let type = 0;
 let page = 1;
 let stop = false;
-let timer = undefined;
+let rank_timer = undefined;
+let plaza_timer = undefined;
+let my_props = {};
 
-function call(url, data, callback) {
-    let http = new XMLHttpRequest();
-    http.open('POST', url, true);
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    http.send(data);
-    http.onreadystatechange = function() {
-        if (http.readyState !== 4 || http.status !== 200) {
-            return;
+async function post(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data
+    });
+    return response.json().then(json => {
+        if (json.code == 10000 && json.data !== undefined) {
+            return json.data;
         }
 
-        let json = {};
-        try {
-            json = JSON.parse(http.responseText);
-        } catch (error) {
-            return;
+        if (json.code == 10201) {
+            return {};
         }
 
-        if (json.code === 10000) {
-            if (callback) {
-                callback(json.data);
-            }
-        } else {
-            stop = true;
-            clearTimeout(timer);
-            console.log('手残了, 请就医');
-            console.table(json);
-        }
-    }
+        stop_all();
+        console.log('手残了, 请就医');
+        console.table(json);
+        return {};
+    });
 }
 
 function get_ranks() {
     let url = 'https://huodong.weibo.cn/hongbao2021/aj_getrank?t=' + type + '&page=' + page;
-    call(url, '', function (data) {
+    post(url).then(data => {
         console.log('get_ranks, type:%d, page:%d, count:%d, has_next:%s', type, page, data.rank.length, data.hasnext);
         data.rank.forEach((rank, i) => {
             setTimeout(() => {
@@ -48,10 +44,50 @@ function get_ranks() {
 
         if (data.hasnext) {
             page++;
-            timer = setTimeout(() => {
+            rank_timer = setTimeout(() => {
                 get_ranks();
             }, data.rank.length * 2000);
         }
+    });
+}
+
+function get_plaza() {
+    let url = 'https://huodong.weibo.cn/hongbao2021/aj_propnews?page=' + page;
+    post(url).then(data => {
+        console.log('get_plaza, page:%d, count:%d, has_next:%s', type, page, data.list.length, data.hasnext);
+        data.list.forEach((user_prop, i) => {
+            if (my_props[user_prop.propId] === undefined) {
+                setTimeout(() => {
+                    get_home(user_prop);
+                }, i * 2000);
+            }
+        });
+
+        if (data.hasnext) {
+            page++;
+            plaza_timer = setTimeout(() => {
+                get_plaza();
+            }, data.list.length * 2000);
+        }
+    });
+}
+
+function get_user_props(uid) {
+    for (let index = 1; index <= 8; index++) {
+        get_props(uid, index);
+    }
+}
+
+function get_props(uid, location) {
+    let url = "https://huodong.weibo.cn/hongbao2021/aj_proplist?location=" + location + "&bull_uid=" + uid;
+    post(url).then(data => {
+        if (data.props === undefined || data.props.length === 0) {
+            return;
+        }
+        
+        data.props.forEach(prop => {
+            my_props[prop.propid] = prop.title;
+        });
     });
 }
 
@@ -61,13 +97,25 @@ function get_home(rank) {
     }
 
     let url = "https://huodong.weibo.cn/hongbao2021/aj_bullhome?bullid=" + rank.bullid + "&debug=false&uid=" + rank.uid;
-    call(url, {}, function (data) {
-        get_touch(rank, data.pagetoken);
+    post(url).then(data => {
+        get_touch(rank, data);
     });
 }
 
-function get_touch(rank, token) {
+function get_touch(rank, data) {
     if (stop) {
+        return;
+    }
+
+    let username = ""
+    if (rank.username) {
+        username = rank.username;
+    } else {
+        username = data.currentUid.id;
+    }
+
+    if (data.canTouchMore === 0) {
+        console.log("%s 的牛 %s 不在", username, data.userBull.bull_name);
         return;
     }
 
@@ -76,21 +124,61 @@ function get_touch(rank, token) {
     query.append('touchnum', 50);
     query.append('touchbullid', rank.bullid);
     query.append('touchuid', rank.uid);
-    query.append('pagetoken', token);
-    query.toString();
-    call(url, query.toString(), function (data) {
+    query.append('pagetoken', data.pagetoken);
+    post(url, query.toString()).then(data => {
         if (data.layer) {
-            console.log("摸到 %s 的 %s", rank.username, data.layer.propGotLayer.desc);
+            console.log("摸到 %s 的 %s", username, data.layer.propGotLayer.desc);
+            // my_props[data.layer.propGotLayer.propid] = data.layer.propGotLayer.desc;
+            mblog_after(data.layer.propGotLayer.aj_pdata, rank);
         } else if(data.touchTips && data.touchTips.length > 0) {
-            console.log("摸过 %s 了, 提示: '%s'", rank.username, data.touchTips[0].text);
+            console.log("摸过 %s 了", username);
             if (data.touchTips[0].text.indexOf("摸一摸道具数已达上限") > 0) {
-                stop = true;
-                clearTimeout(timer);
+                stop_all();
                 console.log("今天不能再摸了");
             }
         } else {
-            console.log("白摸 %s 了", rank.username);
+            console.log("白摸 %s 了", username);
+        } c 
+    });
+}
+
+function get_map(type) {
+    let url = 'https://huodong.weibo.cn/hongbao2021/aj_travelmap?type=' + type;
+    post(url).then(data => {
+        let length = 0;
+        if (data.bottomData !== undefined) {
+            length = data.bottomData.length;
+            data.bottomData.forEach((user, i) => {
+                setTimeout(() => {
+                    get_home(user);
+                }, i * 2000);
+            });
         }
+
+        console.log('get_map, type:%d, count:%d', type, length);
+        type++;
+        if (type < 4) {
+            setTimeout(() => {
+                get_map(type);
+            }, length * 2000);
+        }
+    });
+}
+
+function stop_all() {
+    stop = true;
+    clearTimeout(rank_timer);
+    clearTimeout(plaza_timer);
+}
+
+function mblog_after(sign, rank) {
+    let url = "https://huodong.weibo.cn/hongbao2021/aj_mblogfollow";
+    let query = new URLSearchParams();
+    query.append('sign', sign);
+    query.append('mblog', 1);
+    query.append('follow', 1);
+    post(url, query.toString()).then(data => {
+        console.log("发微博感谢了 %s", rank.username);
     });
 }
 
@@ -99,6 +187,14 @@ if (window.location.href.indexOf("https://huodong.weibo.cn/hongbao2021") === -1)
         window.location = "https://huodong.weibo.cn/hongbao2021";
     }
 } else {
-    console.log("开始摸一摸...");
+    //get_user_props(0);
+
+    console.log("开始摸榜单...");
     get_ranks();
+
+    // console.log("开始摸广场...");
+    // get_plaza();
+
+    console.log("开始摸地图...");
+    get_map(0);
 }
